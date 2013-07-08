8ab3249574c3d9f92a11d0ef337c02474fb315ee
Artur Bartczak <artur.bartczak@code4.pl>
2013-07-09 01:26:15 +0200

    Platform layout and breadcrumbs

---
+++ src/views/menus/breadcrumbs/layout.blade.php
@@ -0,0 +1,24 @@
+
+<ul class="breadcrumb">
+
+@foreach ($menuCollection[0]->all() as $menuItem)
+
+    @if ($menuItem->isActive())
+    <li class="active">
+        @else
+    <li>
+        @endif
+
+{{$menuItem}}
+
+@if ($menuCollection[0]->isLast($menuItem))
+    <span class="divider">
+        <i class="icon-angle-right"></i>
+    </span>
+@endif
+
+    </li>
+
+@endforeach
+
+</ul><!--.breadcrumb-->
