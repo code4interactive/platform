782605f1c1e07ad1fa31f52f07a64989c3a1e263
Artur Bartczak <artur.bartczak@code4.pl>
2013-07-08 22:00:39 +0200

    Administration controller test

---
+++ src/controllers/AdministrationController.php
@@ -0,0 +1,16 @@
+<?php
+
+class AdministrationController extends BaseController {
+
+    /**
+    * Show the profile for the given user.
+    */
+    public function showProfile($id)
+    {
+        //$user = User::find($id);
+
+        echo "aaa";
+        //return View::make('user.profile', array('user' => $user));
+    }
+
+}
